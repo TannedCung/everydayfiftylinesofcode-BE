5c306118bb4be8e5c9ce9e01cfbb2879c1a1a81a
TannedCung <tando1903@gmail.com>
2024-11-19 00:29:05 +0700

    Remove redundant files

--- core/github_services/apis.py
+++
@@ -1,9 +0,0 @@
-# core/github_service.py
-
-import requests
-
-def fetch_github_data(user):
-    headers = {"Authorization": f"token {user.social_auth.get().extra_data['access_token']}"}
-    # Fetch commit data
-    response = requests.get(f"https://api.github.com/users/{user.username}/events", headers=headers)
-    # Process response and calculate commits and modifications
