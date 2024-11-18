42f0ee6324064ce924dc41252fe77147857698f2
TannedCung <tando1903@gmail.com>
2024-11-19 00:29:43 +0700

    Add: endpoint to get commits

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,11 @@
 # api/urls.py
 from django.urls import path
 from .views.login import user_profile, github_callback
+from .views.user_activity import get_user_commits
 
 urlpatterns = [
     path('profile/', user_profile, name='user-profile'),
     path('accounts/github/callback/', github_callback, name='github-callback'),
-
-    
+    path('github/commits/', get_user_commits, name='get_user_commits'),
 ]
 
--- core/views/user_activity.py
+++ core/views/user_activity.py
@@ -1,9 +1,21 @@
 # core/views.py
 
 from rest_framework import viewsets
-from .models import Challenge, UserActivity
-from .serializers import ChallengeSerializer, UserActivitySerializer
+from core.models.user_activity import UserActivity
+from core.serializers.user_activity import UserActivitySerializer
+from core.utils.github import fetch_github_commits
+import requests
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from rest_framework.decorators import api_view
 
 class UserActivityViewSet(viewsets.ModelViewSet):
     queryset = UserActivity.objects.all()
     serializer_class = UserActivitySerializer
+
+@api_view(['GET'])
+def get_user_commits(request):
+    user = request.user
+    commits = fetch_github_commits(user)
+    print(f"[DEBUG]: commit", commits)
+    return JsonResponse(commits, safe=False)
