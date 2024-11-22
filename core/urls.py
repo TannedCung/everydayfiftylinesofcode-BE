8c0f74afdb9df669a6e602075db2a5516ee36c00
TannedCung <tando1903@gmail.com>
2024-11-22 23:20:04 +0700

    Add: urls

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,18 @@
-# api/urls.py
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from .views.login import user_profile, github_callback
-from .views.user_activity import get_user_commits
+from .views.user_activity import get_user_commits, GitHubEventViewSet, GitHubCommitViewSet, GithubFileChangeViewSet
 
+# Define a router for the viewsets
+router = DefaultRouter()
+router.register(r'github/events', GitHubEventViewSet, basename='event')
+router.register(r'github/commits', GitHubCommitViewSet, basename='commit')
+router.register(r'github/changes', GithubFileChangeViewSet, basename='change')
+
+# Add other non-viewset endpoints
 urlpatterns = [
     path('profile/', user_profile, name='user-profile'),
     path('accounts/github/callback/', github_callback, name='github-callback'),
-    path('github/commits/', get_user_commits, name='get_user_commits'),
+    path('github/user_commits/', get_user_commits, name='get_user_commits'),
+    path('', include(router.urls)),  # Include the router-generated routes
 ]
-
