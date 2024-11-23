4545b2f1a6e9aef80a7b8ae353e365f8b1841bc1
TannedCung <tando1903@gmail.com>
2024-11-23 15:51:52 +0700

    Add: challenge endpoints

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.login import user_profile, github_callback
 from .views.user_activity import get_user_commits, GitHubEventViewSet, GitHubCommitViewSet, GithubFileChangeViewSet
+from .views.challenges import ChallengeViewSet
 
 # Define a router for the viewsets
 router = DefaultRouter()
@@ -9,10 +10,12 @@ router.register(r'github/events', GitHubEventViewSet, basename='event')
 router.register(r'github/commits', GitHubCommitViewSet, basename='commit')
 router.register(r'github/changes', GithubFileChangeViewSet, basename='change')
 
+router.register(f'challenge', ChallengeViewSet, basename='challenge')
+
 # Add other non-viewset endpoints
 urlpatterns = [
     path('profile/', user_profile, name='user-profile'),
     path('accounts/github/callback/', github_callback, name='github-callback'),
-    path('github/user_commits/', get_user_commits, name='get_user_commits'),
+    path('github/sync_commits/', get_user_commits, name='get_user_commits'),
     path('', include(router.urls)),  # Include the router-generated routes
 ]
