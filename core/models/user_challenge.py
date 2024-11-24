3c9425b8462baca6ac92fb12e96227e1f3f1a25d
TannedCung <tando1903@gmail.com>
2024-11-24 11:03:09 +0700

    Add: user challenge table

--- core/models/user_challenge.py
+++ core/models/user_challenge.py
@@ -1,9 +1,18 @@
 from django.contrib.auth.models import User
 from django.db import models
 from core.models.challenge import Challenge
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class UserChallenge(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
     start_date = models.DateField()
-    progress = models.IntegerField(default=0)
+    highest_streak = models.PositiveIntegerField(default=0)
+    progress = models.PositiveIntegerField(
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    progress_detail = models.JSONField(default=dict)  # Added JSONField for detailed progress tracking
+
+    def __str__(self):
+        return f"{self.user.username} - {self.challenge.title} - {self.progress}%"
--- core/serializers/user_challenge.py
+++ core/serializers/user_challenge.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from core.models.user_challenge import UserChallenge
+from core.models.challenge import Challenge  # Assuming Challenge is imported from core
 
 class UserChallengeSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserChallenge
-        fields = ['id', 'user', 'challenge', 'progress', 'start_date']
+        fields = ['id', 'challenge', 'start_date', 'progress', 'progress_detail', 'highest_streak']  # Include all fields
+        read_only_fields = ('progress', 'progress_detail', 'start_date', 'highest_streak')  # Progress is updated by the system
